9086ec01323749dc26a7b14e039bbc6d638b24e5
mthdht <mthdht@gmail.com>
2018-07-29 22:34:25 +0200

    made first version of card component => test state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Card';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        test
+      <div className="App ">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
       </div>
     );
   }
