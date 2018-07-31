ea3d99b004dd2ac65ec4f4467a2acaa90076b769
mthdht <mthdht@gmail.com>
2018-07-31 14:13:51 +0200

    finish basic card click handle => change card status and add correct match

--- src/Board.js
+++ src/Board.js
@@ -5,23 +5,74 @@ class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: this.generateCards()
+            cards: this.generateCards(),
+            pairs: [],
+            currentPair: []
         };
     }
 
     generateCards() {
         const cards = [];
+
         this.props.colors.forEach((color, index) => {
-            cards.push(<Card face={color} status={'hidden'} key={index}/>)
+            cards.push({face: color, status: 'hidden', id: index});
         });
 
         return cards;
     };
 
+    getStatus(index) {
+        const {currentPair, pairs,cards} = this.state;
+
+        if (currentPair.includes(cards[index]) || pairs.includes(cards[index])) {
+            return "visible";
+        } else return "hidden"
+    }
+
+    handleCardClick = (index) => {
+        const {cards, currentPair} = this.state;
+
+        if (currentPair.length === 2) {
+            return;
+        }
+
+        if (currentPair.length === 0) {
+            this.setState({
+                currentPair: [cards[index]]
+            });
+            return;
+        }
+
+        if (currentPair.length === 1) {
+            this.handlePairAttempt(index);
+        }
+
+    };
+
+    handlePairAttempt(index) {
+        const {cards, currentPair, pairs} = this.state;
+        currentPair.push(cards[index]);
+        this.setState({currentPair});
+
+        if (currentPair[0].face === currentPair[1].face && currentPair[0].id !== currentPair[1].id) {
+            this.setState({
+                pairs: [...pairs, ...currentPair],
+                currentPair: []
+            });
+        } else {
+            setTimeout(() => this.setState({cards, currentPair: []}), 1000);
+        }
+    }
+
     render() {
+        const cards = [];
+        this.state.cards.forEach((card) => {
+            cards.push(<Card face={card.face} status={this.getStatus(card.id)} index={card.id} key={card.id} handleCardClick={this.handleCardClick}/>);
+        });
+
         return (
             <div className={"board w3-row"}>
-                {this.state.cards}
+                {cards}
             </div>
         )
     }
--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Card extends Component {
 
+    handleClick(index) {
+        if (this.props.status !== "visible") {
+            this.props.handleCardClick(index);
+        } else console.log("already chosen");
+    }
+
     render() {
         return (
-            <div className="w3-col m2 w3-padding-large">
+            <div className="w3-col m2 w3-padding-large" onClick={() => this.handleClick(this.props.index)}>
                 <div className={"card w3-round w3-" +
                 (this.props.status === 'hidden' ? 'gray' : this.props.face)}></div>
             </div>
